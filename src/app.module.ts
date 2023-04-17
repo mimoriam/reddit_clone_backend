5b1f72f11e422d4e2b2c0bdcd94e0ec793e0d4dc
Mimo <mimoriam420@gmail.com>
2023-04-18 00:13:08 +0500

    Commit #2 - Dotenv configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+      isGlobal: true,
+      expandVariables: true,
+      cache: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import { NestFactory } from '@nestjs/core';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app: INestApplication = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestApplication = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:3000'],
+      credentials: true,
+    },
+  });
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('/api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // disableErrorMessages: true,
+      // forbidNonWhitelisted: true,
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.use(cookieParser());
+
+  await app.listen(configService.get('APP_PORT') || 3001);
 }
 
 bootstrap().then();
