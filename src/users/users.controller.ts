9cab9ee22246b5a554e22692ef6884439d39c8b9
Mimo <mimoriam420@gmail.com>
2023-04-18 02:14:28 +0500

    Commit #5 - Role based ACL/Authorization working for just 1 role

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Auth } from '../iam/authentication/decorators/auth.decorator';
+import { AuthType } from '../iam/authentication/enums/auth-type.enum';
+import { RolesGuard } from '../iam/authorization/guards/roles.guard';
+import { Roles } from '../iam/authorization/decorators/roles.decorator';
+import { Role } from '@prisma/client';
 
 @Controller('users')
+@UseGuards(RolesGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @Auth(AuthType.Bearer)
+  @Roles(Role.ADMIN)
+  async checkForAuthorization() {
+    return {
+      message: 'Authorized',
+    };
+  }
 }
